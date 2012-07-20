f45c5b416ca91a174a7a17ff3cd85f5dd1bda1af
Enrico Thies <enrico.thies@gmail.com>
2012-07-20 21:03:22 +0200

    renamed

--- test/sfChoiceFormat-test.js
+++ test/sfChoiceFormat-test.js
@@ -13,19 +13,3 @@ vows.describe('format_number_choice').addBatch({
 		}
 	}
 }).export(module);
-
-
-/*
-
-
-var test;
-
-(test = function() {
-  var match, plurals, sCF;
-  plurals = ['[0]nothing found', '[1]one found', '{2,3,4,5}2 or ... or 5 found', '(5,10)6-9 found', '[10,15)10-14 found', '[15,20]15-20 found', '(30,+Inf] more the 30 found', '[-10,0)less than 0 found', '{n:(n%6 == 0)} mod(6) found', '{n:(1)} anything found'].join('|');
-  sCF = new sfChoiceFormat();
-  for (match = -32; match <= 32; match++) {
-    document.writeln(match + ': ' + sCF.format(plurals, match) + '<br />');
-  }
-})();
-*/

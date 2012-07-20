6cb33940fcc683316334599ecfd42b1623d9a73d
Enrico Thies <enrico.thies@gmail.com>
2012-07-20 20:41:00 +0200

    test try

--- test/sfChoiceFormat.test.js
+++ test/sfChoiceFormat.test.js
@@ -1,3 +1,23 @@
+var assert = require('assert'),
+	vows = require('vows'),
+	formatChoice = require('../')
+	plurals = ['[0]nothing found', '[1]one found', '{2,3,4,5}2 or ... or 5 found', '(5,10)6-9 found', '[10,15)10-14 found', '[15,20]15-20 found', '(30,+Inf] more the 30 found', '[-10,0)less than 0 found', '{n:(n%6 == 0)} mod(6) found', '{n:(1)} %count% found'].join('|')
+	;
+
+vows.describe('format_number_choice').addBatch({
+	'When providing 0':{
+		topic: formatChoice.format_number_choice(0, {'%count%': 0}, 0)
+		, 'result should be "nothing found"': function (result) {
+			assert.isString(result);
+			assert.equal(result, 'nothing found');
+		}
+	}
+}).export(module);
+
+
+/*
+
+
 var test;
 
 (test = function() {
@@ -8,3 +28,4 @@ var test;
     document.writeln(match + ': ' + sCF.format(plurals, match) + '<br />');
   }
 })();
+*/
